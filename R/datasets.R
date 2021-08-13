7361e64a5c2e034a06dba5174a911a48370b2c30
Dirk Wulff <dwulff@users.noreply.github.com>
2021-08-13 12:27:15 +0200

    ..

---
+++ R/datasets.R
@@ -0,0 +1,55 @@
+#' SDG queries of the Aurora Universities Network
+#'
+#' A dataset containing the SDG queries version 5.0 of the \href{https://aurora-network.global/activity/sustainability/}{Aurora Universities Network}. See the corresponding \href{https://github.com/Aurora-Network-Global/sdg-queries}{GitHub repository}. For the actual implementation of the queries see \code{aurora_simple}, \code{aurora_and}, \code{aurora_w}, and the queries hard-coded on \code{detect_aurora}.
+#'
+#' @format A data frame with 378 rows and 5 columns
+#' \describe{
+#'   \item{sdg}{Label of the SDG}
+#'   \item{sdg_title}{Title of the SDG}
+#'   \item{sdg_description}{Description of the SDG}
+#'   \item{query_id}{Index of the query}
+#'   \item{query}{Original SDG query}
+#' }
+"aurora_queries"
+
+#' SDG queries of SIRIS Academic
+#'
+#' A dataset containing the SDG queries of \href{http://www.sirislab.com/lab/sdg-research-mapping/}{SIRIS Academic}. The queries are available from\href{https://zenodo.org/record/3567769#.YRY9xdMzY8N}{Zenodo.org}. For the actual implementation of the queries see \code{aurora_simple}, \code{aurora_and}, \code{aurora_w}, and the queries hard-coded on \code{detect_aurora}.
+#'
+#' @format A data frame with 378 rows and 5 columns
+#' \describe{
+#'   \item{sdg}{Label of the SDG}
+#'   \item{keyword}{Primary SDG query element}
+#'   \item{extra}{Secodary SDG query element}
+#'   \item{query_id}{Index of the query}
+#'   \item{query}{SDG query}
+#' }
+"siris_queries"
+
+#' SDG queries of Elsevier
+#'
+#' A dataset containing the SDG queries of \href{https://www.elsevier.com/connect/sdg-report}{Elsevier}. The queries are available from \href{https://data.mendeley.com/datasets/87txkw7khs/1}{data.mendeley.com}.
+#'
+#' @format A data frame with 378 rows and 5 columns
+#' \describe{
+#'   \item{sdg}{Label of the SDG}
+#'   \item{query_id}{Index of the query}
+#'   \item{query}{SDG query}
+#' }
+"elsevier_queries"
+
+#' SDG keyword ontology of Bautista-Puig and Mauleón
+#'
+#' A dataset containing the SDG queries based on the keyword ontology of Bautista-Puig and Mauleón. The queries are available from \href{https://figshare.com/articles/dataset/SDG_ontology/11106113/1}{figshare.com}.
+#'
+#' Bautista-Puig, N.; Mauleón E. (2019). Unveiling the path towards sustainability: is there a research interest on sustainable goals? In the 17th International Conference on Scientometrics & Informetrics (ISSI 2019), Rome (Italy), Volume II, ISBN 978-88-3381-118-5, p.2770-2771.
+#'
+#' @format A data frame with 378 rows and 5 columns
+#' \describe{
+#'   \item{sdg}{Label of the SDG}
+#'   \item{keyword}{SDG keyword used in query}
+#'   \item{query_id}{Index of the query}
+#'   \item{query}{SDG query}
+#' }
+"ontology_queries"
+
--- R/detect.R
+++ R/detect.R
@@ -1,66 +1,79 @@
 #' Detect SDGs in text
 #'
-#' `detect_sdg` identifies SDGs in text using the Aurora, Elsevier, and Siris query systems.
+#' \code{detect_sdg} identifies SDGs in text using SDG query systems developed by the Aurora Universities Network, SIRIS Academic, and Elsevier.
 #'
-#' @param corpus object of class \code{tCorpus} containing text.
-#' @param verbose logical.
+#' \code{detect_sdg} implements three SDG query systems developed by the Arora Universities Network (see \code{\link{aurora_queries}}), SIRIS Academic (see \code{\link{siris_queries}}), and Elsevier (see \code{\link{elsevier_queries}}), and one keyword-based system by Bautista-Puig and Mauleón labeled Ontology (see \code{\link{ontology_queries}}). `detect_sdg` calls dedicated \code{detect_*} for each of the four system. Search of the Lucene-style Boolean queries and the keywords is implemented using the \code{\link[corpustools]{search_features}} function from the \href{https://cran.r-project.org/package=corpustools}{\code{corpustools}} package.
+#'
+#' By default, \code{detect_sdg} runs only the three query systems, as they are considerably less liberal than the keyword-based Ontology and therefore likely produce more valid SDG classifications. Users should be aware that systematic validations and comparison between the systems are still largely lacking. Consequently, any results should be interpreted with a high level caution.
+#'
+#' @param text \code{character} vector or object of class \code{tCorpus} containing text in which SDGs shall be detected.
+#' @param system \code{character} vector specifying the query systems to be used. Can be one or more of \code{"aurora"}, \code{"siris"}, \code{"elsevier"}, and \code{"ontology"}. By deafult all but \code{"ontology"} are used.
+#' @param output \code{character} specifying the level of detail in the output. The default \code{"features"} returns a \code{tibble} with one row per matched query, include a variable containing the features of the query that were matched in the text. By contrast, \code{"docs"} returns an aggregated \code{tibble} with one row per matched sdg, without information on the featurs.
+#' @param verbose \code{logical} specifying whether messages on the function's progress should be printed.
+#'
+#' @return The function returns a tibble containing the SDG hits found in the vector of documents. Depending on the value of \code{output} the tibble will contain all or some of the following columns:
+#' \describe{
+#'  \item{document}{Index of the element in \code{text} where match was found.}
+#'  \item{sdg}{Label of the SDG found in document.}
+#'  \item{system}{The name of the query system that produced the match.}
+#'  \item{query_id}{Index of the query within the query system that produced the match.}
+#'  \item{features}{Concatenated list of words that caused the query to match.}
+#'  \item{hit}{Index of hit for a given system.}
+#' }
+#'
+#'
 #'
 #' @export
 
-detect_sdg = function(text, system = c("aurora","elsevier","siris"), out = c("features","docs"), verbose = TRUE){
-
-  if(class())
+detect_sdg = function(text, system = c("aurora","siris","elsevier"), output = c("features","docs"), verbose = TRUE){
 
   # make corpus
-  if(class(text) != "tCorpus"){
-    corpus = make_corpus(input[[i]])
-    } else {
+  if(class(text) == "character"){
+    corpus = make_corpus(text)
+    } else if(class(text) == "tCorpus"){
     corpus = input[[i]]
+    } else {
+    stop("Argument text must be either class character or corpustools::tCorpus.")
     }
 
   #make output list
   hits = list()
 
   # run sdg
+  if (!all(system %in% c("aurora","elsevier","siris", "ontology"))){
+    stop("Argument system must be aurora, elsevier, siris or ontology.")
+    }
   if("aurora" %in% system){
     if(verbose) cat("Running aurora queries\n",sep = '')
-    hits_aurora = detect_aurora(corpus)
-    hits[["aurora"]] <- hits_aurora }
-  if("elsevier" %in% system){
-    if(verbose) cat("Running elsevier queries\n",sep = '')
-    hits_elsevier = detect_elsevier(corpus)
-    hits[["elsevier"]] <- hits_elsevier}
+    hits[["aurora"]] = detect_aurora(corpus)}
   if("siris" %in% system) {
     if(verbose) cat("Running siris queries\n",sep = '')
-    hits_siris = detect_siris(corpus)
-    hits[["siris"]] <- hits_siris}
+    hits[["siris"]] = detect_siris(corpus)}
+  if("elsevier" %in% system){
+    if(verbose) cat("Running elsevier queries\n",sep = '')
+    hits[["elsevier"]] = detect_elsevier(corpus)}
   if("ontology" %in% system) {
     if(verbose) cat("Running ontology queries\n",sep = '')
-    hits_ontology = detect_ontology(corpus)
-    hits[["ontology"]] <- hits_ontology}
-  if (!any(system %in% c("aurora","elsevier","siris", "ontology"))){
-    stop("system must be aurora, elsevier, siris or ontology")
-    }
+    hits[["ontology"]] = detect_ontology(corpus)}
 
   #combine lists to df
   hits <- do.call(rbind, hits)
 
   # reduce if requested
-  if(out[1] == "docs"){
+  if(output[1] == "docs"){
     v = hits_df %>%
       dplyr::group_by(document, sdg, system) %>%
       dplyr::summarize(hits = dplyr::n()) %>%
       dplyr::ungroup()
     } else {
-    if(out[1] != "features") stop("out must be features or docs")
+    if(output[1] != "features") stop('Argument output must be "features" or "docs"')
     }
 
   #convert document to factor for downstream functions
   hits <- hits %>%
     dplyr::mutate(document = factor(document, levels = 1:length(unlist(input))))
 
-
-  # out
+  #output
   hits %>%
     dplyr::arrange(document, sdg, system)
 

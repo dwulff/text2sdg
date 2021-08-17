81d95a2bf32a748534cb9c3b0ba0d4aae9269509
Dominik Meier <dominikmeier@outlook.com>
2021-08-17 16:07:34 +0200

    correct comp_phi

--- R/comp_phi.R
+++ R/comp_phi.R
@@ -36,65 +36,68 @@ comp_phi <- function(hits,
   #handle selected SDGs
   sdgs = paste0("SDG-", ifelse(show_sdg < 10, "0", ""),show_sdg) %>% sort()
   hits <- hits %>%
-    filter(sdg %in% sdgs) %>%
-    filter(system %in% show_system)
+    dplyr::filter(sdg %in% sdgs) %>%
+    dplyr::filter(system %in% show_system)
 
 
 
   if(compare[[1]] == "systems") {
 
-    phi_dat <- tidyr::expand_grid(document = 1:length(levels(hits$document)), system = show_system, sdg = unique(hits$sdg)) %>%
-      dplyr::mutate(document = as.factor(document)) %>%
-      dplyr::left_join(hits %>% dplyr::select(document, system, sdg, hit)) %>%
-      dplyr::mutate(hit = dplyr::if_else(is.na(hit), FALSE, TRUE)) %>%
-      dplyr::distinct() %>%
-      dplyr::arrange(document, sdg) %>%
-      tidyr::pivot_wider(names_from = system, values_from = hit) %>%
-      `[`(,-(1))
+
+
+      phi_dat <- tidyr::expand_grid(document = 1:length(levels(hits$document)), system = show_system, sdg = unique(hits$sdg)) %>%
+        dplyr::mutate(document = as.factor(document)) %>%
+        dplyr::left_join(hits %>% dplyr::select(document, system, sdg, hit)) %>%
+        dplyr::mutate(hit = dplyr::if_else(is.na(hit), FALSE, TRUE)) %>%
+        dplyr::distinct() %>%
+        dplyr::arrange(document, sdg) %>%
+        tidyr::pivot_wider(names_from = system, values_from = hit) %>%
+        `[`(,-(1))
 
 
 
-    comp_phi_internal <- function(x) {
+      comp_phi_internal <- function(x) {
 
-      x %>%
-        split(.$sdg) %>%
-        purrr::map(~ dplyr::select(., -sdg)) %>%
-        purrr::map(~ .x %>%  dplyr::mutate_at(dplyr::everything(.), function(x) factor(x, levels = c(TRUE, FALSE)))) %>%
-        purrr::map(~ table(.)) %>%
-        purrr::map(psych::phi) %>%
-        dplyr::as_tibble() %>%
-        tidyr::pivot_longer(dplyr::everything())
+        x %>%
+          split(.$sdg) %>%
+          purrr::map(~ dplyr::select(., -sdg)) %>%
+          purrr::map(~ .x %>%  dplyr::mutate_at(dplyr::everything(.), function(x) factor(x, levels = c(TRUE, FALSE)))) %>%
+          purrr::map(~ table(.)) %>%
+          purrr::map(psych::phi) %>%
+          dplyr::as_tibble() %>%
+          tidyr::pivot_longer(dplyr::everything())
 
-    }
-
-
-    phis <- list()
-    i <- 1
-    for(idx in as.data.frame(utils::combn(ncol(phi_dat) - 1,2))) {
-
-      a <- comp_phi_internal(phi_dat[,c(1,idx + 1)])
-      a["comparison"] <- paste(names(phi_dat[, idx + 1]), collapse = " - ")
-      print(paste(names(phi_dat[, idx + 1]), collapse = " - "))
-
-      phis[[i]] <- a
-      i = i + 1
-
-    }
-
-    #output = 1 df
-    #change name and value column names.
-    #change NaNs to NA
-    phis <- do.call(rbind, phis)
-
-    #convert nans to NA
-    phis <- phis %>%
-      dplyr::mutate(value = dplyr::if_else(is.nan(value), NA_real_, value)) %>%
-      dplyr::rename(sdg = name,
-                    phi = value)
+      }
 
 
+      phis <- list()
+      i <- 1
+      for(idx in as.data.frame(utils::combn(ncol(phi_dat) - 1,2))) {
 
-    return(phis)
+        a <- comp_phi_internal(phi_dat[,c(1,idx + 1)])
+        a["comparison"] <- paste(names(phi_dat[, idx + 1]), collapse = " - ")
+        print(paste(names(phi_dat[, idx + 1]), collapse = " - "))
+
+        phis[[i]] <- a
+        i = i + 1
+
+      }
+
+      #output = 1 df
+      #change name and value column names.
+      #change NaNs to NA
+      phis <- do.call(rbind, phis)
+
+      #convert nans to NA
+      phis <- phis %>%
+        dplyr::mutate(value = dplyr::if_else(is.nan(value), NA_real_, value)) %>%
+        dplyr::rename(sdg = name,
+                      phi = value)
+
+
+
+      return(phis)
+
 
 
 
@@ -105,26 +108,89 @@ comp_phi <- function(hits,
       warning("There were no SDG hits in the data, returning empty data frame.")
       return(tibble())
     }
-    sdg_cor <- hits %>%
-      dplyr::distinct(document, sdg, system) %>%
-      widyr::pairwise_cor(sdg, document, sort = TRUE) %>%
-      dplyr::arrange(item1, item2) %>%
-      tidyr::pivot_wider(names_from = item2, values_from = correlation) %>%
-      dplyr::relocate(c(item1, `SDG-01`)) %>%
-      dplyr::rename(SDG = item1)
+    # sdg_cor <- hits %>%
+    #   dplyr::distinct(document, sdg, system) %>%
+    #   widyr::pairwise_cor(sdg, document, sort = TRUE) %>%
+    #   dplyr::arrange(item1, item2) %>%
+    #   tidyr::pivot_wider(names_from = item2, values_from = correlation) %>%
+    #   dplyr::relocate(c(item1, `SDG-01`)) %>%
+    #   dplyr::rename(SDG = item1)
+    #
+    #
+    #
+    # return(sdg_cor)
 
 
+    comp_phi_internal <- function(x) {
 
-    return(sdg_cor)
+      x %>%
+        split(.$system) %>%
+        purrr::map(~ dplyr::select(., -system)) %>%
+        purrr::map(~ .x %>%  dplyr::mutate_at(dplyr::everything(.), function(x) factor(x, levels = c(TRUE, FALSE)))) %>%
+        purrr::map(~ table(.)) %>%
+        purrr::map(psych::phi) %>%
+        dplyr::as_tibble() %>%
+        tidyr::pivot_longer(dplyr::everything())
+
+    }
+
+
+    phi_dat <- tidyr::expand_grid(document = 1:length(levels(hits$document)), system = show_system, sdg = sdgs) %>%
+      dplyr::mutate(document = as.factor(document)) %>%
+      dplyr::left_join(hits %>% dplyr::select(document, system, sdg, hit)) %>%
+      dplyr::mutate(hit = dplyr::if_else(is.na(hit), FALSE, TRUE)) %>%
+      dplyr::distinct() %>%
+      dplyr::arrange(document, sdg) %>%
+      tidyr::pivot_wider(names_from = sdg, values_from = hit) %>%
+      `[`(,-(1))
+
+
+    phis <- list()
+    i <- 1
+    for(idx in as.data.frame(utils::combn(ncol(phi_dat) - 1,2))) {
+      a <- comp_phi_internal(phi_dat[,c(1,idx + 1)])
+      a["comparison"] <- paste(names(phi_dat[, idx + 1]), collapse = " - ")
+
+      phis[[i]] <- a
+      i = i + 1
+
+    }
+
+    phis <- do.call(rbind, phis)
+
+    #convert nans to NA
+    phis <- phis %>%
+      dplyr::mutate(value = dplyr::if_else(is.nan(value), NA_real_, value)) %>%
+      dplyr::rename(system = name,
+                    phi = value,
+                    association = comparison)
+
+    sdg_table <- tidyr::expand_grid(sdg1 = sdgs, sdg2 = sdgs, system = show_system)
+
+
+    phis_join <- phis %>%
+      dplyr::mutate(sdg1 = substr(association, 1, 6),
+                    sdg2 = substr(association, 10,15)) %>%
+      dplyr::filter(system == "elsevier")
+
+
+    phis <- sdg_table %>%
+      dplyr::left_join(phis_join, by = c("sdg1" = "sdg1", "sdg2" = "sdg2", "system" = "system")) %>%
+      dplyr::left_join(phis_join, by = c("sdg1" = "sdg2", "sdg2" = "sdg1", "system" = "system")) %>%
+      dplyr::mutate(phi = dplyr::if_else(is.na(phi.x), phi.y, phi.x),
+             phi = dplyr::if_else(sdg1 == sdg2, 1, phi)) %>%
+      dplyr::select(-c(association.x, association.y, phi.x, phi.y)) %>%
+      dplyr::arrange(system, sdg1) %>%
+      tidyr::pivot_wider(names_from = sdg2, values_from = phi)
+
+
+    return(phis)
+
+
 
   }
 
 }
-
-
-
-
-
 
 
 

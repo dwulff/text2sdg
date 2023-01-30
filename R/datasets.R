4ac654793b56b1c98b081cc0eca75a30ccf7cadf
Dirk Wulff <dirk.wulff@gmail.com>
2023-01-30 08:40:46 +0100

    new pkgdown

--- R/datasets.R
+++ R/datasets.R
@@ -40,7 +40,7 @@
 
 #' Ensemble models based on a random forest architecture that pools the predictions of six labeling systems generated using `detect_sdg_systems()` and also considers text length.
 #'
-#' A list of trained `ranger::ranger()` random forest models that are used as the basis of `detect_sdg`.
+#' A list of trained `ranger::ranger()` random forest models that are used by `detect_sdg()`.
 #'
 #' @source Wulff, Meier, & Mata (2023)
 "ensembles"
